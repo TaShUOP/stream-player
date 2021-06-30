582d8f6b6630bee8a0e13b30f99bbbc3460d8faf
Ankur Das <55151728+TaShUOP@users.noreply.github.com>
2021-06-30 13:48:11 +0530

    Updated Stream player
    Stream player added files and sub-branched!

--- src/components/pages/stream.js
+++ src/components/pages/stream.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Player from '../player'
-import '../player.css'
 
 function stream() {
   return (
--- src/components/player.js
+++ src/components/player.js
@@ -4,28 +4,26 @@ import './player.css'
 
 function Player() {
   //const Src = 'http://YOURSERVERIP:PORT/hls/show.m3u8'
-  const [data, setData] = useState('http://YOURSERVERIP:PORT/hls/.m3u8')
+  const [data, setData] = useState('http://3.108.128.205:8080/hls/.m3u8')
   //const test = 'abcd'
   const Copy = (val) => {
     //console.log('http://YOURSERVERIP:PORT/hls/' + val.target.value + '.m3u8')
-    setData('http://YOURSERVERIP:PORT/hls/' + val.target.value + '.m3u8')
+    setData('http://3.108.128.205:8080/hls/' + val.target.value + '.m3u8')
   }
   return (
     <>
-      <div className='play'>
-        <ReactHlsPlayer
-          src={data}
-          hlsConfig={{
-            maxLoadingDelay: 4,
-            minAutoBitrate: 0,
-            lowLatencyMode: true,
-          }}
-          autoPlay={true}
-          controls={true}
-          width='100%'
-          height='auto'
-        ></ReactHlsPlayer>
-      </div>
+      <ReactHlsPlayer
+        src={data}
+        hlsConfig={{
+          maxLoadingDelay: 4,
+          minAutoBitrate: 0,
+          lowLatencyMode: true,
+        }}
+        autoPlay={true}
+        controls={true}
+        width='100%'
+        height='auto'
+      ></ReactHlsPlayer>
       <div className='data'>
         <label className='name'>Stream ID:</label>
         <input type='text' onChange={Copy} className='box'></input>
