3303d8aedc07bb554de9e1f616f123d6a6a06c96
Ankur Das <55151728+TaShUOP@users.noreply.github.com>
2021-11-04 00:34:55 +0530

    update stream

--- src/components/pages/stream.js
+++ src/components/pages/stream.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Player from '../player'
-import Navbar from '../Navbar'
 //import HeroSection from '../HeroSection'
 
 function stream() {
   return (
     <>
-      <Navbar />
       <Player />
     </>
   )
