d4583a68b572f754b9807343a821a8f992c294dd
Ankur Das <55151728+TaShUOP@users.noreply.github.com>
2021-11-08 22:51:19 +0530

    Update Player.js

--- src/components/player.js
+++ src/components/player.js
@@ -5,31 +5,29 @@ import './player.css'
 function Player() {
   //const Src = 'http://YOURSERVERIP:PORT/hls/show.m3u8'
   const [data, setData] = useState(
-    'https://b1e7-103-80-62-104.in.ngrok.io/hls/.m3u8'
+    'https://5f9c-103-80-62-197.in.ngrok.io/hls/.m3u8'
   )
   //const test = 'abcd'
   const Copy = (val) => {
     //console.log('http://YOURSERVERIP:PORT/hls/' + val.target.value + '.m3u8')
     setData(
-      'https://b1e7-103-80-62-104.in.ngrok.io/hls/' + val.target.value + '.m3u8'
+      'https://5f9c-103-80-62-197.in.ngrok.io/hls/' + val.target.value + '.m3u8'
     )
   }
   return (
     <>
-      <div className='stream'>
-        <ReactHlsPlayer
-          src={data}
-          hlsConfig={{
-            maxLoadingDelay: 4,
-            minAutoBitrate: 0,
-            lowLatencyMode: true,
-          }}
-          autoPlay={true}
-          controls={true}
-          width='100%'
-          height='auto'
-        ></ReactHlsPlayer>
-      </div>
+      <ReactHlsPlayer
+        src={data}
+        hlsConfig={{
+          maxLoadingDelay: 4,
+          minAutoBitrate: 0,
+          lowLatencyMode: true,
+        }}
+        autoPlay={true}
+        controls={true}
+        width='800'
+        height='auto'
+      ></ReactHlsPlayer>
       <div className='data'>
         <label className='name'>Stream ID:</label>
         <input type='text' onChange={Copy} className='box'></input>
